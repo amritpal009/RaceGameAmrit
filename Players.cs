1629a835d6a0823b70c43424f9d2ef33cb274ccd
AVI KHEHRA <AVI KHEHRA@DESKTOP-5NAR473>
2021-02-08 18:32:02 +1300

    Add project files.

---
+++ Players.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaceGameAmrit
+{
+    using System.Windows.Forms;
+
+    
+   
+        public class Avi : Punter // player1
+        {
+            public Avi(Bet MyBet, Label MaximumBet, int Money, Label MyLabel) : base(MyBet, MaximumBet, Money, MyLabel)
+            {
+            }
+
+            public override void setPunterName()
+            {
+                Title = "Avi";
+            }
+        }
+
+        public class Gourav : Punter  // player2
+    {
+            public Gourav(Bet bet, Label MaximumBet, int Money, Label label) : base(bet, MaximumBet, Money, label)
+            {
+            }
+
+            public override void setPunterName()
+            {
+                Title = "Gourav";
+            }
+        }
+
+        public class Vicky : Punter // player3
+    {
+            public Vicky(Bet bet, Label MaximumBet, int Money, Label label) : base(bet, MaximumBet, Money, label)
+            {
+            }
+
+            public override void setPunterName()
+            {
+                Title = "Vicky";
+            }
+        }
+    }
+
---
+++ PunterFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaceGameAmrit
+{
+    public class PunterFactory
+    {
+        public Punter getPunter(String Name, Label MaximumBet, Label bet)
+        {
+            Punter p;
+            switch (Name.ToLower())
+            {
+                case "avi":
+                    p = new Avi(null, MaximumBet, 50, bet);
+                    break;
+
+                case "gourav":
+                    p = new Gourav(null, MaximumBet, 50, bet);
+                    break;
+
+                case "vicky":
+                    p = new Vicky(null, MaximumBet, 50, bet);
+                    break;
+
+                default:
+                    p = null;
+                    break;
+            }
+            p.setPunterName();
+            return p;
+        }
+    }
+}
